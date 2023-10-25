be5a6976342d0370f8b02a4693dedd63852595e4
Jeremy <barycki@student.agh.edu.pl>
2023-10-25 18:59:54 +0200

    m

--- Party_Animals/src/main/java/agh/ics/oop/World.java
+++ Party_Animals/src/main/java/agh/ics/oop/World.java
@@ -4,8 +4,7 @@ import agh.ics.oop.model.MoveDirections;
 
 public class World {
     private static void run(MoveDirections[] args){
-        /*int i =0;
-        String tekst="";*/
+
         System.out.println("Start");
         for(MoveDirections arg:args){
             String message = switch (arg) {
@@ -18,23 +17,7 @@ public class World {
             if(message!=null){
                 System.out.println(message);
             }
-            /*String message = switch (arg){
-                case "f" -> "Zwierzak idzie do przodu";
-                case "b" -> "Zwierzak idzie do tyłu";
-                case "r" -> "Zwierzak idzie w prawo";
-                case "l" -> "Zwierzak idzie w lewo";
-                default -> null;
-            };
-            System.out.println(message);
-            */
-            /*tekst+=arg;
-            if (i< args.length-1){
-                tekst+=",";
-            }*/
             }
-            //i++;
-
-        //System.out.println(tekst);
         System.out.println("Stop");
     }
     public static void main(String[] args) {
